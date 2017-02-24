6311996eedc5933564d6b1f0026e3f111cd0f48e
xuyusir <xuyusir@163.com>
2017-02-24 17:00:49 +0800

    基本实现
    基本实现
    修复大多bug

--- app/src/main/java/yuyu/mymedia/MainActivity.java
+++ app/src/main/java/yuyu/mymedia/MainActivity.java
@@ -4,32 +4,25 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import  android.app.*;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import  android.util.*;
 import android.widget.Toast;
-import android.database.*;
-import  android.content.ContentResolver;
-import  android.provider.*;
 import java.io.IOException;
 
 public class MainActivity extends Activity {
     private MediaPlayer mp;
     private SurfaceView sv;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=getIntent();
+        Intent intent=this.getIntent();
         final String datapath=intent.getStringExtra("datap");
-        Toast.makeText(getApplicationContext(), "get ok please click play",Toast.LENGTH_SHORT).show();
         DisplayMetrics metric = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metric);
         final int pw= metric.widthPixels;     // 屏幕宽度（像素）
@@ -40,6 +33,7 @@ public class MainActivity extends Activity {
         Button select=(Button)findViewById(R.id.select_media);
         final Button pause=(Button)findViewById(R.id.pause);
         Button stop=(Button)findViewById(R.id.stop);
+
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
@@ -52,40 +46,37 @@ public class MainActivity extends Activity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 try {
                     mp.setDataSource(datapath);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
                 mp.setDisplay(sv.getHolder());
                 try {
                     mp.prepare();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
                 RelativeLayout.LayoutParams rl= new RelativeLayout.LayoutParams(mp.getVideoWidth(),mp.getVideoHeight());
                 rl.addRule(RelativeLayout.CENTER_HORIZONTAL);
                 sv.setLayoutParams(rl);
+
                 mp.start();
             }
         });
 
-
        select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ContentResolver c=getContentResolver();
-                Cursor cur = c.query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, null,
-                                     null, null,null);
-                int num=cur.getCount();
-                cur.moveToFirst();
-                for (int i=0;i<num;i++){
-                    String datapath=cur.getString(cur.getColumnIndex(MediaStore.Video.Media.DATA));
-                    cur.moveToNext();
-                    Log.e("TAG", datapath);
+                Intent intent=new Intent(MainActivity.this,MyListView.class);
+                startActivity(intent);
+                finish();
 
                 }
-            }
+
         });
 
         pause.setOnClickListener(new View.OnClickListener() {
@@ -101,25 +92,19 @@ public class MainActivity extends Activity {
                     pause.setText("pause");
 
                 }
+
             }
         });
-
 
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//            mp.stop();
-//            mp.reset();
-                Intent intent=new Intent(MainActivity.this,MyListView.class);
-                startActivity(intent);
+            mp.stop();
+            mp.reset();
 
             }
         });
 
-
-
-
-
-
     }
+
 }
--- app/src/main/java/yuyu/mymedia/MyListView.java
+++ app/src/main/java/yuyu/mymedia/MyListView.java
@@ -26,24 +26,23 @@ public class MyListView extends Activity {
         listView = new ListView(this);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,getData()));
         setContentView(listView);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent=new Intent(MyListView.this,MainActivity.class);
                 intent.putExtra("datap",getData().get(position));
                 startActivity(intent);
+                Toast.makeText(getApplicationContext(), "get ok please click play",Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
-
-
-
 
     }
 
     private List<String> getData(){
 
         List<String> data = new ArrayList<String>();
-
         ContentResolver c=getContentResolver();
         Cursor cur = c.query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, null,
                 null, null,null);
@@ -54,9 +53,7 @@ public class MyListView extends Activity {
             data.add(datapath);
             cur.moveToNext();
         }
-
         return data;
     }
-
 
 }
